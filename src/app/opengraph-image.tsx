b581ccd35a9ad291085210edc4497ed133b2576d
Olaf <olafnieliwodzki@wp.pl>
2023-12-27 22:29:57 +0100

    Opengraph

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,6 +1,4 @@
-import {Anton} from 'next/font/google';
 import {ImageResponse} from 'next/og';
-const anton = Anton({weight: '400', subsets: ['latin']});
 
 // Image metadata
 export const alt = 'TalentHunting - Recruitment as a Service';
@@ -9,30 +7,11 @@ export const size = {
     height: 630,
 };
 
-export const contentType = 'image/png';
+export const contentType = 'image/jpg';
 
 // Image generation
 export default async function Image() {
     return new ImageResponse(
-        (
-            // ImageResponse JSX element
-            <div
-                className={`${anton.className}`}
-                style={{
-                    fontSize: 128,
-                    background: 'white',
-                    width: '100%',
-                    height: '100%',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
-                <span className='text-mainBlue'>{'TALENT'}</span><span>{'HUNTING'}</span>
-            </div>
-        ),
-        {
-            ...size,
-        },
+        <img src={'opg.jpg'} alt={alt} width={size.width} height={size.height} />,
     );
 }
