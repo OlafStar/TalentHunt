258e3a725dcb3e66de3071b392f91a0faae95213
Olaf <olafnieliwodzki@wp.pl>
2023-12-27 22:32:12 +0100

    Fix path

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,5 +1,4 @@
 import {ImageResponse} from 'next/og';
-
 // Image metadata
 export const alt = 'TalentHunting - Recruitment as a Service';
 export const size = {
@@ -12,6 +11,13 @@ export const contentType = 'image/jpg';
 // Image generation
 export default async function Image() {
     return new ImageResponse(
-        <img src={'opg.jpg'} alt={alt} width={size.width} height={size.height} />,
+        (
+            <img
+                src={'../../public/opg.jpg'}
+                alt={alt}
+                width={size.width}
+                height={size.height}
+            />
+        ),
     );
 }
